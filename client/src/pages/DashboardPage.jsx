63e3aae52cb8fd12470c4c48556003b65183111d
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2024-05-29 21:14:03 +0530

    Dashboard UI added Venky

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import PersonIcon from '@mui/icons-material/Person';
+import BadgeIcon from '@mui/icons-material/Badge';
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 
 const Container = styled.div`
   max-width: 600px;
@@ -11,20 +18,71 @@ const Container = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  transition: color 0.3s, transform 0.3s;
+
+  &:hover {
+    color: #3498db;
+    transform: translateX(5px);
+  }
+`;
+
 const DashboardPage = () => {
   return (
     <Container>
       <h2>Dashboard</h2>
-      <nav>
-        <ul>
-          <li><Link to="/transactions">View Transactions</Link></li>
-          <li><Link to="/balance">Check Balance</Link></li>
-          <li><Link to="/add-money">Add Money</Link></li>
-          <li><Link to="/add-transaction">Add Transaction</Link></li>
-          <li><Link to="/profile">View Profile</Link></li>
-          <li><Link to="/badges">View Badges</Link></li>
-        </ul>
-      </nav>
+      <List>
+        <StyledLink to="/transactions">
+          <ListItem button>
+            <ListItemIcon>
+              <CompareArrowsIcon />
+            </ListItemIcon>
+            <ListItemText primary="View Transactions" />
+          </ListItem>
+        </StyledLink>
+        <StyledLink to="/balance">
+          <ListItem button>
+            <ListItemIcon>
+              <AccountBalanceWalletIcon />
+            </ListItemIcon>
+            <ListItemText primary="Check Balance" />
+          </ListItem>
+        </StyledLink>
+        <StyledLink to="/add-money">
+          <ListItem button>
+            <ListItemIcon>
+              <AttachMoneyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add Money" />
+          </ListItem>
+        </StyledLink>
+        <StyledLink to="/add-transaction">
+          <ListItem button>
+            <ListItemIcon>
+              <AddBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add Transaction" />
+          </ListItem>
+        </StyledLink>
+        <StyledLink to="/profile">
+          <ListItem button>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="View Profile" />
+          </ListItem>
+        </StyledLink>
+        <StyledLink to="/badges">
+          <ListItem button>
+            <ListItemIcon>
+              <BadgeIcon />
+            </ListItemIcon>
+            <ListItemText primary="View Badges" />
+          </ListItem>
+        </StyledLink>
+      </List>
     </Container>
   );
 };
