f006b1227b779b6c7298e2f2ab5e9ba265d4281b
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2024-05-30 09:00:18 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -65,7 +65,6 @@ const Navbar = () => {
         <NavItem whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <NavLink to="/balance">Balance</NavLink>
         </NavItem>
-        {/* Add more NavItem and NavLink for other pages */}
         <NavItem>
           <LogoutButton>
             <Logout />
